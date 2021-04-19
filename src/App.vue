26e9bd599eb420ba06741e8824802ef2e2c84dc6
sriharisudarshan <66459791+sriharisudarshan@users.noreply.github.com>
2021-04-19 21:37:09 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/ico1.png">
+    <img alt="Vue logo" src="./assets/unnamed.png">
     <HelloWorld msg="Welcome to Digital triplet"/>
   </div>
 </template>
